ed3974254d73b0b4d1b266de33195a146e750644
Johannes Kamprad <archlinux@kamprad.net>
2024-02-19 22:09:08 +0100

    prepare.sh added script to hack in yay builds..

--- prepare.sh
+++ prepare.sh
@@ -6,9 +6,14 @@ wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://raw.githubusercont
 # Get wallpaper for installed system
 wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/endeavouros-theming/master/backgrounds/endeavouros-wallpaper.png"
 
-
 # Make sure build scripts are executable
 chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}
+
+# prebuild AUR packages
+
+yay -S --makepkgconf ~/Documents/hyprland/EndeavourOS-Community-hyprland-ISO/makepkg.conf --noconfirm cava wlogout nwg-look-bin swww networkmanager-dmenu-bluetoothfix-git
+cp ~/Documents/hyprland/EndeavourOS-Community-hyprland-ISO/aurbuilds/* ~/Documents/hyprland/EndeavourOS-Community-hyprland-ISO/airootfs/root/packages/
+sudo pacman -Rsc --noconfirm networkmanager-dmenu-bluetoothfix-git swww wlogout nwg-look-bin cava
 
 # Build liveuser skel
 cd "airootfs/root/endeavouros-skel-liveuser"
